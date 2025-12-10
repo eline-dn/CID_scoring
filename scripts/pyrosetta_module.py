6f2ddb601b750d576fd6112d8f5656400797b2f3
eline-dn <eline.denis@gmail.com>
2025-12-10 20:36:04 +0100

    Update pyrosetta_module.py

--- scripts/pyrosetta_module.py
+++ scripts/pyrosetta_module.py
@@ -1,4 +1,65 @@
 # import pyrosetta_utils.py
+SCRIPT_PATH = os.path.dirname(__file__)
+sys.path.append(f"{SCRIPT_PATH}/../functions")
+from pyrosetta_utils import *
 # load dependancies
+import os,  sys
+import numpy as np
+import argparse
 
 # one binder scoring function:
+def score_target_n_binder(pdb):
+  data={}
+  pose=load_pose(pdbfile, assert_lig=False)
+  # interchain contacts computations:
+  data["atom_contact_BT1"]=count_chain_atom_contacts(pose=pose, ch_id1=1, ch_id2=2, delta=0.2)
+  data["atom_contact_BT2"]= get_atomic_contact_data(pose)
+  data["atom_contact_BT3"]= count_atomic_contacts(pdb=pdb, chain1=1, chain2=2, delta=0.2)
+  interface_scores, interface_AA, interface_residues_pdb_ids_str=score_nolig_interface(pdb_file=pdb, interface= "A_B", binder_chain="B")
+  data={**data, **interface_scores}
+  return data
+
+def score_ternary_complex(pdb):
+  data={}
+  pose=load_pose(pdbfile, assert_lig=True) # load pose and check that the last residue in the pose is a ligand
+  # derive binary pose from the ternary complex: (i.e. ligand + binder)
+  binder_start = pose.conformation().chain_begin(2)
+  lb_pose = pyrosetta.rosetta.core.pose.Pose()
+  pyrosetta.rosetta.core.pose.append_subpose_to_pose(lb_pose, pose, binder_start, pose.size(), 1)
+  
+  # interchain contacts computations:
+  data["atom_contact_BL1"]=count_chain_atom_contacts(pose=pose, ch_id1=2, ch_id2=3, delta=0.2)
+  data["atom_contact_BL2"]= get_atomic_contact_data(lb_pose) # this one is probably going to separate both and give zero, do not lend it to much confidence
+  data["atom_contact_BL3"]= count_atomic_contacts(pdb=pdb, chain1=2, chain2=3, delta=0.2)
+  interface_scores, interface_AA, interface_residues_pdb_ids_str=score_lig_interface(pdb_file, interface= "AF_B", binder_chain="B")
+  data={**data, **interface_scores}
+  return data  
+
+
+######## 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pdb", nargs="+", type=str, help="List of Input PDBs") # list of pdb files 
+parser.add_argument("--mk_params", type=bool, help=" need to create a Params files in the pdb files'dir? Set to true if the pdbs are ternary complexes. in that case, add a smiles for the ligand")
+parser.add_argument("--smiles", required=False, type=str, help="smiles used for ligand parametrization, eg c1cc(oc1)CNc2cc(c(cc2C(=O)O)S(=O)(=O)N)Cl for the FUN ligand")
+parser.add_argument("--lig_name", required=False, type=str, help="name used for ligand parametrization, eg  FUN ligand")
+args = parser.parse_args()
+
+
+#  init pyrosetta and parametrize ligand (or not):
+if args.mk_params:
+  params=create_param(smiles=args.smiles, pdb_file=args.pdb[0], lig_name=args.lig_name)
+  pyr_init(params=params)
+else: 
+  pyr_init() # initialise without the param file
+
+for pdb in args.pdb:
+  id=
+  df=
+  if mk_params:
+    data=score_ternary_complex(pdb)
+  else: 
+    data=score_target_n_binder(pdb)
+  # write to csv
+
+    
