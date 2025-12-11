d3c89a72837e92d13e2a2a9aec63f8c589953907
eline-dn <eline.denis@gmail.com>
2025-12-11 14:11:44 +0100

    Update AF3_output_module.py

--- scripts/AF3_output_module.py
+++ scripts/AF3_output_module.py
@@ -32,7 +32,7 @@ parser.add_argument("--AF3_outs", nargs="+", type=str, help="List of AF3 output 
 #parser.add_argument("--smiles", required=False, type=str, help="smiles used for ligand parametrization, eg c1cc(oc1)CNc2cc(c(cc2C(=O)O)S(=O)(=O)N)Cl for the FUN ligand")
 parser.add_argument("--lig_name", required=False, type=str, help="name used for ligand parametrization, eg  FUN ligand")
 parser.add_argument("--outdir", required=False, type=str, help="output folder for csv file")
-#parser.add_argument("--prefix", required=False, type=str, help="prefix for csv file")
+parser.add_argument("--prefix", required=False, type=str, help="prefix for csv file")
 args = parser.parse_args()
 
 if (args.lig_name is not None):
@@ -44,19 +44,51 @@ else:
   ternary=False
   metrics_prefix="bin_"
   
-outdir=args.outdir
+if args.outdir is not None:
+  outdir=args.outdir
+else: 
+  outdir="."
+
+if args.prefix is not None:
+  prefix=args.prefix
+else: 
+  prefix=""
 
 
-##### prepare inputs in the same folder, everything repredicted in one aF3 command
-for pdb in args.pdb:
-  id=os.path.basename(pdb).replace(".pdb", "")
-  structure=load_PDB(pdb)
-  seq_binder=chain2seq(structure, "B", make_str=True)
-  json=prep_binder_input_json(template_path=target_json, outdir=outdir ,id=id ,seq_binder=seq_binder)
+for dir in args.AF3_outs:
+  id=dir.split("/")[-1]
+  # extract confidence metrics:
+  confidence=dir + "/" +id+"_summary_confidences.json"
+  data=extract_af3_confidence_metrics(confidence)
+  # find reference:
+  for pdb in args.ref_pdb:
+    if id in pdb:
+      ref_pdb_file=pdb
+  # load the structures:
+  ref= load_PDB(ref_pdb_file)
+  mov= load_CIF(os.path.join(dir,f"{id}_model.cif"))
+  # chain structure
+  mapping={"A":"A", "B":"B", "L":args.lig_name}
+  # compute RMSDs
+  if ternary:
+    rmsds=ternary_RMSDs(ref, mov, mapping)
+  else:
+    rmsds=binary_RMSDs(ref, mov, mapping)
+  # add a prefix on the metrics names:
+  data = {f"{metrics_prefix}{k}": v for k, v in data.items()}
+  rmsds = {f"{metrics_prefix}{k}": v for k, v in rmsds.items()}
+  # add the id:
+  data["id"]=id
+  # save in csv
+  df=pd.df([{**data, **rmsds}])
+  csv_path=f"{outdir}/{prefix}_AF3_{metrics_prefix}reprediction_metrics.csv"
+  df.to_csv(csv_path, mode="a", index=False, header=not pd.io.common.file_exists(csv_path))
+    
+  
 
 
 elapsed_time = time.time() - script_start_time
 elapsed_text = f"{'%d hours, %d minutes, %d seconds' % (int(elapsed_time // 3600), int((elapsed_time % 3600) // 60), int(elapsed_time % 60))}"
-n_binder=len(args.pdb)
+n_binder=len(args.AF3_outs)
 print(f"Finished structure reprediction output scoring for {n_binder} complexes. Script execution took: "+elapsed_text)
 
