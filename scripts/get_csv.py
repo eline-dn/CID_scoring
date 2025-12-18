f56558c878f5f6d5b1e526830407676cfb574dde
eline-dn <eline.denis@gmail.com>
2025-12-18 13:24:21 +0100

    Create get_csv.py

---
+++ scripts/get_csv.py
@@ -0,0 +1,6 @@
+# get csvs
+# clean them up: 
+# - ensure col "id" from binder_id eg. 
+# - ensure t2_2_7_3_t0.1_ltp0.2_dcut15.0_model0 or t2_2_7_4_t0.3_ltp0.3_dcut8.0_model becomes =>  t2_2_7_3_t0.1_ltp0.2_dcut15.0
+# - put prefix on col names to ensure the source: 
+
