523aa19cb0598976f0ca6899cc7e1bfc8449a735
eline-dn <eline.denis@gmail.com>
2026-01-07 12:58:50 +0100

    Update run_af3_input.sh

--- run_af3_input.sh
+++ run_af3_input.sh
@@ -7,19 +7,25 @@
 #SBATCH --partition h100
 #SBATCH --time 10:00:00
 #SBATCH --account lpdi
-#SBATCH --job-name cd2
+#SBATCH --job-name af3input
 
 # setup working directory
 SDIR="/work/lpdi/users/eline/CID_scoring/"
-WDIR="/work/lpdi/users/eline/CID/1Z9Y_FUN_p2/" # change project dir here
+#WDIR="/work/lpdi/users/eline/CID/1Z9Y_FUN_p2/" # change project dir here
+WDIR= "$1"
+target_id= "$2"
+target_template="$3"
+smiles="$4" 
+lig_name="$5"
 
 cd "$WDIR"
 
 #------------ af3 binary reprediction:-----------------
 # input preparation:
-python "$SDIR/scripts/AF3_input_module.py"  --structure ./input/binder_refs/*.pdb  --target_id 1Z9Y --target_template "$WDIR/input/1Z9Y_target_template.cif"  --outdir "$WDIR/output/af3binary/json"
+python "$SDIR/scripts/AF3_input_module.py"  --structure ./input/binder_refs/*.pdb  --target_id "$target_id" --target_template "$target_template"  --outdir "$WDIR/output/af3binary/json"
 
 
 #------------ af3 ternary reprediction:-----------------
 # input preparation:
-python "$SDIR/scripts/AF3_input_module.py"  --structure ./input/binder_refs/*.pdb  --target_id 1Z9Y --target_template "$WDIR/input/1Z9Y_target_template.cif"  --smiles "c1cc(oc1)CNc2cc(c(cc2C(=O)O)S(=O)(=O)N)Cl" --lig_name FUN --outdir "$WDIR/output/af3ternary/json"
+python "$SDIR/scripts/AF3_input_module.py"  --structure ./input/binder_refs/*.pdb  --target_id "$target_id" --target_template "$target_template"  --smiles "$smiles" --lig_name "$lig_name" --outdir "$WDIR/output/af3ternary/json"
+# "c1cc(oc1)CNc2cc(c(cc2C(=O)O)S(=O)(=O)N)Cl"
